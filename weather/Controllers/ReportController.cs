d6ab21884dd97c8a79b6d21525a68f1a97f8e705
Brian Hanafee <bhanafee@gmail.com>
2017-10-14 00:29:11 -0700

    Add weather demo .NET Core MVC

---
+++ weather/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using weather.Models;
+
+namespace weather.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReportController : Controller
+    {
+
+        [HttpGet("{zip}", Name = "GetReport")]
+        public IActionResult GetByZip(int zip)
+        {
+            var result = new Report("Sunny");
+            return new ObjectResult(result);
+        }
+    }
+}
---
+++ weather/Models/Report.cs
@@ -0,0 +1,14 @@
+﻿namespace weather.Models
+{
+    public class Report
+    {
+        private readonly string _content;
+
+        public Report(string content)
+        {
+            _content = content;
+        }
+
+        public string Content => _content;
+    }
+}
