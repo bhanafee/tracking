9b92767ce6d891451d1f3edb882302a7029b82de
Brian Hanafee <bhanafee@gmail.com>
2017-12-11 07:53:39 -0800

    Use WebMVC with basic security and templates

--- web/src/main/java/com/example/demo/Track.java
+++ web/src/main/java/com/example/demo/Track.java
@@ -3,11 +3,11 @@ package com.example.demo;
 import io.opentracing.ActiveSpan;
 import io.opentracing.Tracer;
 import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Track {
 
     private final Logger log;
