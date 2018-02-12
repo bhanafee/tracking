323fa935f11388c0dd6315cc12394aa47e0ad5bb
Brian Hanafee <bhanafee@gmail.com>
2018-02-11 22:58:25 -0800

    Fix download location and wget options

--- geolocation/db/src/main/sh/download.sh
+++ geolocation/db/src/main/sh/download.sh
@@ -6,13 +6,13 @@ export CONGRESS=$2
 export CENSUS=$3
 
 # States
-wget --no-clobber https://www2.census.gov/geo/tiger/TIGER${YEAR}/STATE/tl_${YEAR}_us_state.zip
+wget -c https://www2.census.gov/geo/tiger/TIGER${YEAR}/STATE/tl_${YEAR}_us_state.zip
 
 # Counties
-wget --no-clobber https://www2.census.gov/geo/tiger/TIGER${YEAR}/COUNTY/tl_${YEAR}_us_county.zip
+wget -c https://www2.census.gov/geo/tiger/TIGER${YEAR}/COUNTY/tl_${YEAR}_us_county.zip
 
 # Congressional districts
-wget --no-clobber https://www2.census.gov/geo/tiger/TIGER${YEAR}/CD/tl_${YEAR}_us_cd${CONGRESS}.zip
+wget -c https://www2.census.gov/geo/tiger/TIGER${YEAR}/CD/tl_${YEAR}_us_cd${CONGRESS}.zip
 
 # ZIP code tabulation areas
-wget --no-clobber https://www2.census.gov/geo/tiger/TIGER${YEAR}/ZCTA5/tl_${YEAR}_us_zcta5${CENSUS}.zip
+wget -c https://www2.census.gov/geo/tiger/TIGER${YEAR}/ZCTA5/tl_${YEAR}_us_zcta5${CENSUS}.zip
