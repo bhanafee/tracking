93c8ccd852cadf49f2bee97831ed42c47987fdc3
Brian Hanafee <bhanafee@gmail.com>
2018-02-14 07:57:59 -0800

    Add shp2psql step using python  to stage shapefiles from /gisdata

--- geolocation/db/src/main/sh/stage.sh
+++ geolocation/db/src/main/sh/stage.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-# find shapefile zips
-# unzip into staging area
-# shp2psql
+python3 -m zipfile -e /gisdata/*state.zip /stage/
+python3 -m zipfile -e /gisdata/*county.zip /stage/
+
+shp2pgsql -s 4269:4326 -g geom -W "latin1" /stage/*_state.shp tiger_staging.state >> /docker-entrypoint-initdb.d/zdata.sql
+shp2pgsql -s 4269:4326 -g geom -W "latin1" /stage/*_county.shp tiger_staging.county >> /docker-entrypoint-initdb.d/zdata.sql
+
+rm /stage/*
