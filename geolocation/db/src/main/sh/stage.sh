22244b3ddc39f4488b44bd65916ef58b2067c491
Brian Hanafee <bhanafee@gmail.com>
2018-02-15 19:55:03 -0800

    Parameterization

--- geolocation/db/src/main/sh/stage.sh
+++ geolocation/db/src/main/sh/stage.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-python3 -m zipfile -e /gisdata/*state.zip /stage/
-python3 -m zipfile -e /gisdata/*county.zip /stage/
+UNZIP="python3 -m zipfile -e"
+CONVERT="shp2pgsql -s 4269:4326 -g geom -W 'latin1'"
+SOURCES=(state county)
+TARGET=${SCRIPTS}zdata.sql
 
-shp2pgsql -s 4269:4326 -g geom -W "latin1" /stage/*_state.shp tiger_staging.state >> /docker-entrypoint-initdb.d/zdata.sql
-shp2pgsql -s 4269:4326 -g geom -W "latin1" /stage/*_county.shp tiger_staging.county >> /docker-entrypoint-initdb.d/zdata.sql
+cd $STAGE
 
-rm /stage/*
+for TABLE in "${SOURCES[@]}"
+do
+  $UNZIP /gisdata/*_${TABLE}.zip .
+  $CONVERT *_${TABLE}.shp tiger_staging.${TABLE} >> $TARGET
+done
